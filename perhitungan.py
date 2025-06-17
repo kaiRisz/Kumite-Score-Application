c2cf223a0d506a7d9bfe29f28fbc86f0ac3f286a
achiralucy <achiralucy@gmail.com>
2025-06-17 13:06:53 +0700

    Update perhitungan.py

--- perhitungan.py
+++ perhitungan.py
@@ -134,3 +134,133 @@ class KarateScoringApp:
     def aka_sub(self):
         self.aka_score = max(0, self.aka_score - 1)
         self.aka_score_label.config(text=str(self.aka_score), font=("Helvetica", 200))
+        
+    #method biar pas button back dipencet tampilan score balik ke menu awal
+    def go_back(self):
+        for widget in self.root.winfo_children():
+            widget.destroy()
+        Menu(self.root, self.db)
+
+    #method buat ngatur timernya
+    def toggle_timer(self):
+        if not self.running and not self.running2: #ini start
+            self.start_time = time.time() - self.elapsed_time
+            self.start_time2 = time.time() - self.elapsed_time2
+            self.running = True
+            self.running2 = True
+            self.start_btn.config(text="Pause")
+        else: #ini pause
+            self.elapsed_time = time.time() - self.start_time
+            self.elapsed_time2 = time.time() - self.start_time2
+            self.running = False
+            self.running2 = False
+            self.start_btn.config(text="Start")
+    
+    #method untuk ngatur waktu juga enih pas udh 3 menit yea kak
+    def update_timer(self):
+        if self.running:
+            self.elapsed_time = time.time() - self.start_time
+            if self.elapsed_time >= 180:
+                self.running = False
+                self.running2 = False
+                self.start_btn.config(text="Start")
+                messagebox.showinfo("Time's up", "3 minutes have passed. Time is up!")
+            minutes = int(self.elapsed_time // 60)
+            seconds = int(self.elapsed_time % 60)
+            self.stopwatch_label.config(text=f"{minutes}:{seconds:02}")
+        if self.running2:
+            self.elapsed_time2 = time.time() - self.start_time2
+            minutes2 = int(self.elapsed_time2 // 60)
+            seconds2 = int(self.elapsed_time2 % 60)
+            self.stopwatch2_label.config(text=f"{minutes2}:{seconds2:02}")
+        self.root.after(1000, self.update_timer)
+
+    #untuk munculin sama ilangin UI stopwatchnyaaa
+    def toggle_stopwatch(self):
+        if self.stopwatch_visible:
+            self.stopwatch_label.place_forget()
+            self.stopwatch2_label.place_forget()
+        else:
+            self.stopwatch_label.place(x=418, y=480)
+            self.stopwatch2_label.place(x=1104, y=480)
+        self.stopwatch_visible = not self.stopwatch_visible
+
+    #method biar pas tombol shikakku dipencet keluar tulisan "Penalty!" di ao
+    def ao_shikkaku(self):
+        self.ao_score_label.config(text="Penalty!", font=("Helvetica", 50))
+
+    #method biar pas tombol kikken dipencet keluar tulisan "Disqualified!" di ao
+    def ao_kikken(self):
+        self.ao_score_label.config(text="Disqualified!", font=("Helvetica", 50))
+
+    #method biar pas tombol shikakku dipencet keluar tulisan "Penalty!" di aka
+    def aka_shikkaku(self):
+        self.aka_score_label.config(text="Penalty!", font=("Helvetica", 50))
+
+     #method biar pas tombol kikken dipencet keluar tulisan "Disqualified!" di aka
+    def aka_kikken(self):
+        self.aka_score_label.config(text="Disqualified!", font=("Helvetica", 50))
+
+    #method buat nampilin skor yang udah didapatkan ke message box
+    def done(self):
+        self.running = False
+        self.running2 = False
+        self.start_btn.config(text="Start")
+
+        ao_name = self.ao_name_entry.get()
+        aka_name = self.aka_name_entry.get()
+        division = self.division_entry.get()
+        judges = self.judges_spinbox.get()
+        ao_score = self.ao_score
+        aka_score = self.aka_score
+
+        # Hitung durasi stopwatch
+        minutes = int(self.elapsed_time // 60)
+        seconds = int(self.elapsed_time % 60)
+        durasi_waktu = f"{minutes}:{seconds:02}"
+
+        result = f"{ao_name} (Ao): {ao_score}\n{aka_name} (Aka): {aka_score}\nJudges: {judges}"
+        messagebox.showinfo("Match Result", result)
+
+        # Simpan ke CSV + database
+        self.simpan_hasil(durasi_waktu)
+
+    #method buat ngambil dan nyimpen skor yang udah didapatkan ke csv
+    def simpan_hasil(self, durasi_waktu):
+        ao_name = self.ao_name_entry.get()
+        aka_name = self.aka_name_entry.get()
+        division = self.division_entry.get()
+        ao_score = self.ao_score
+        aka_score = self.aka_score
+
+        with open("hasil_pertandingan.csv", mode="a", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow(["AO", ao_name, ao_score,"AKA", aka_name, aka_score, durasi_waktu, division])
+
+        # nyimpen ke database file csvnya
+        self.db.tambah_skor(ao_name, ao_score, division)
+        self.db.tambah_skor(aka_name, aka_score, division)
+
+
+    #buat ngembaliin semua tampilan yang udah di jalanin ke setelan awal
+    def reset(self):
+        self.ao_score = 0
+        self.aka_score = 0
+        self.elapsed_time = 0
+        self.elapsed_time2 = 0
+        self.running = False
+        self.running2 = False
+        self.start_btn.config(text="Start")
+        self.stopwatch_label.config(text="0:00", font=("Helvetica", 85))
+        self.stopwatch2_label.config(text="0:00", font=("Helvetica", 85))
+        self.ao_score_label.config(text="0", font=("Helvetica", 200))
+        self.aka_score_label.config(text="0", font=("Helvetica", 200))
+        self.ao_name_entry.delete(0, tk.END)
+        self.aka_name_entry.delete(0, tk.END)
+        self.division_entry.delete(0, tk.END)
+
+#buat munculin jendela baru yang isinya aplikasi scoring karate, terus dia pake data dari db buat nyimpen atau nampilin skor.
+def run_scoring_app(db):
+    root = tk.Toplevel()
+    app = KarateScoringApp(root, db)
+    root.mainloop()
